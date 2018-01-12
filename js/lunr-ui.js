f9dc398830f64acb23b81c6ba3e2b027d11c54b6
mnyrop <marii@riseup.net>
2018-01-12 12:45:35 -0500

    add results with images to top

--- js/lunr-ui.js
+++ js/lunr-ui.js
@@ -4,23 +4,26 @@ $(document).ready(function() {
     var results_div = $('#results');
     var query = $(this).val();
     var results = index.search(query, {expand: true});
+    var sorted = [];
+
+    for (var r in results){
+      var ref = results[r].ref;
+      var obj  = store[ref];
+      if (obj.on_website == 'yes') {sorted.unshift(ref);}
+      else {sorted.push(ref);}
+    }
 
     results_div.empty();
-
-    for (var item in results) {
-
-      var ref   = results[item].ref;
-      var link  = store[ref].link;
-      var title = store[ref].title;
-      var desc  = store[ref].description;
-      var date  = store[ref]._date;
-      var coll  = store[ref].collection;
+    for (var s in sorted) {
+      var idx   = sorted[s];
+      var item  = store[idx];
+      var link  = item.link;
+      var title = item.title;
+      var desc  = item.description;
+      var date  = item._date;
+      var coll  = item.collection;
       var meta  = desc + ' / ' + date + ' / ' + coll;
-
-      if (store[ref].on_website == 'yes') {
-        meta = '&#127748; ' + meta;
-        console.log("TRUE " + meta);
-      }
+      if (item.on_website == 'yes') { meta = '&#127748; ' + meta;}
 
       var result = '<div class="result"><b><a href="' + link + '">' + title + '</a></b><br><p>' + meta +'</p></div>';
       results_div.append(result);

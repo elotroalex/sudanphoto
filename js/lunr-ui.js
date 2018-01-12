45ec2abcb8a2122c7e4e2530164476c5ccdea289
mnyrop <marii@riseup.net>
2018-01-12 13:07:35 -0500

    cap results to 10

--- js/lunr-ui.js
+++ js/lunr-ui.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
   $('input#search').on('keyup', function () {
-
     var results_div = $('#results');
     var query = $(this).val();
     var results = index.search(query, {expand: true});
     var sorted = [];
-
-    for (var r in results){
+    for (var r in results){ // loop to sort results with online images to the front
       var ref = results[r].ref;
       var obj  = store[ref];
       if (obj.on_website == 'yes') {sorted.unshift(ref);}
       else {sorted.push(ref);}
     }
-
     results_div.empty();
-    for (var s in sorted) {
+    if (sorted.length > 10){results_div.prepend("<p><small>Displaying 10 of " + sorted.length + " results.</small></p>");}
+    for (var s in sorted.slice(0, 9)) { // limit visible results to 10
       var idx   = sorted[s];
       var item  = store[idx];
       var link  = item.link;
       var title = item.title;
       var desc  = item.description;
       var date  = item._date;
-      var coll  = item.collection;
+      var coll  = item.collection + " Collection";
       var meta  = desc + ' / ' + date + ' / ' + coll;
-      if (item.on_website == 'yes') { meta = '&#127748; ' + meta;}
-
+      if (item.on_website == 'yes') { meta = '&#127748; ' + meta;} // add image icon if image is online
       var result = '<div class="result"><b><a href="' + link + '">' + title + '</a></b><br><p>' + meta +'</p></div>';
       results_div.append(result);
     }

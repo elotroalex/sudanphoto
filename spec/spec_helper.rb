d847eb673aafd71318fddd9f436332a57e716bde
Marii <mn2802@columbia.edu>
2018-03-14 17:31:08 -0400

    rm no-sandbox

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
 
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { 'args' => %w{headless no-sandbox disable-gpu} }
+      chromeOptions: { 'args' => %w{headless disable-gpu} }
     )
     Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
   end
